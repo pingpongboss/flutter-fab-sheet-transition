9f0824446fac0797ca9f443a0d77d642ad132e70
Mark Wei <markwei@markwei-macbookpro2.roam.corp.google.com>
2015-10-30 16:48:08 -0700

    Initial commit

---
+++ lib/circular_reveal_material.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CircularReveal extends StatelessComponent {
+
+  final double centerX;
+  final double centerY;
+  final double radius;
+  final Widget child;
+
+  CircularReveal({this.child, this.centerX: .5, this.centerY: .5, this.radius:1.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
